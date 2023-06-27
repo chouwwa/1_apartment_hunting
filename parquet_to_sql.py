e30ba1d3be3e29b0d1424191dd6d14fc633a7f65
wsl_waylchan <waylchan@cisco.com>
2023-06-26 17:02:36 -0700

    need to change all vars in parquet sql script

--- parquet_to_sql.py
+++ parquet_to_sql.py
@@ -2,49 +2,78 @@ import pandas as pd
 import numpy as np
 import math
 import time, timeit
+import argparse
 
 from sqlalchemy import create_engine
+
+
+def main(params):
+    user = params.user
+    password = params.password
+    host = params.host
+    port = params.port
+    db = params.db
+    table_name = params.table_name
+    url = params.url
+
+    engine = create_engine("postgresql://root:root@localhost:5432/ny_taxi")
+
+    df = pd.read_parquet("./yellow_tripdata_2021-01.parquet")
+
+    df = df.rename(
+        {
+            "VendorID": "vendorid",
+            "RatecodeID": "ratecodeid",
+            "PULocationID": "pulocationid",
+            "DOLocationID": "dolocationid",
+        },
+        axis=1,
+    )
+
+    def col_astype(col, dtype):
+        df[col] = df[col].astype(dtype)
+
+    col_astype("vendorid", pd.Int16Dtype())
+    col_astype("passenger_count", pd.Int8Dtype())
+    col_astype("ratecodeid", pd.Int8Dtype())
+    col_astype("payment_type", pd.Int8Dtype())
+    col_astype("pulocationid", pd.Int16Dtype())
+    col_astype("dolocationid", pd.Int16Dtype())
+
+    df_chunks = np.array_split(df, math.ceil(df.shape[0] / n_chunks))
+
+    df.head(0).to_sql(name="yellow_taxi_data", con=engine, if_exists="replace")
+
+    for each in df_chunks:
+        start = time.time()
+        each.to_sql(name="yellow_taxi_data", con=engine, if_exists="append")
+        end = time.time()
+
+        print(f"pushed a chunk.......{end - start:.3f}s")
+
 
 pd.set_option("display.max_rows", 15)
 pd.set_option("display.max_column", 10)
 
 n_chunks = 100000
 
-engine = create_engine("postgresql://root:root@localhost:5432/ny_taxi")
-engine.connect()
+parser = argparse.ArgumentParser(description="Ingest Parquet to PostGreSQL")
 
-df = pd.read_parquet("./yellow_tripdata_2021-01.parquet")
+# user
+# password
+# host
+# port
+# database name
+# table name
+# url of the csv
 
-df = df.rename(
-    {
-        "VendorID": "vendorid",
-        "RatecodeID": "ratecodeid",
-        "PULocationID": "pulocationid",
-        "DOLocationID": "dolocationid",
-    },
-    axis=1,
-)
+parser.add_argument("user", help="user for PostGres")
+parser.add_argument("password", help="password for PostGres")
+parser.add_argument("host", help="host for PostGres")
+parser.add_argument("port", help="port for PostGres")
+parser.add_argument("db", help="database name for PostGres")
+parser.add_argument("table_name", help="table name for PostGres")
+parser.add_argument("url", help="url of the csv for PostGres")
 
-
-def col_astype(col, dtype):
-    df[col] = df[col].astype(dtype)
-
-
-col_astype("vendorid", pd.Int16Dtype())
-col_astype("passenger_count", pd.Int8Dtype())
-col_astype("ratecodeid", pd.Int8Dtype())
-col_astype("payment_type", pd.Int8Dtype())
-col_astype("pulocationid", pd.Int16Dtype())
-col_astype("dolocationid", pd.Int16Dtype())
-
-
-df_chunks = np.array_split(df, math.ceil(df.shape[0] / n_chunks))
-
-df.head(0).to_sql(name="yellow_taxi_data", con=engine, if_exists="replace")
-
-for each in df_chunks:
-    start = time.time()
-    each.to_sql(name="yellow_taxi_data", con=engine, if_exists="append")
-    end = time.time()
-
-    print(f"pushed a chunk.......{end - start:.3f}s")
+args = parser.parse_args()
+print(args.accumulate(args.integers))
