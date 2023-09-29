f1b8372eeae3a1de16b4fcfe2192f87209ac6ad8
wsl_waylo <waylon.chan3@gmail.com>
2023-09-28 21:02:41 -0700

    prefect minio credentials not found update warning

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3.10-slim-bullseye
 
 RUN apt-get update \
     && apt-get install -y wget
-RUN pip install prefect pandas sqlalchemy psycopg psycopg2-binary pyarrow
+RUN pip install prefect prefect_aws pandas sqlalchemy psycopg psycopg2-binary pyarrow 
 
 WORKDIR /app
 COPY parquet_to_sql.py parquet_to_sql.py
--- parquet_to_sql.py
+++ parquet_to_sql.py
@@ -4,9 +4,14 @@ import math
 import time, timeit
 import argparse
 import os
+import pyarrow
 
+from pathlib import Path
 from sqlalchemy import create_engine
 from prefect import flow, task
+from prefect.tasks import task_input_hash
+from prefect_aws.s3 import S3Bucket, MinIOCredentials
+from datetime import timedelta
 
 
 @task(name="PG Connection")
@@ -36,7 +41,7 @@ def connection(params):
 
 @task(name="Download CSV")
 def download_csv(url, csv_name):
-    """wgets url and output into output.csv
+    """wgets url and output into 'csv_name' (name of variable)
 
     Args:
         url (str): valid url whole address
@@ -47,9 +52,12 @@ def download_csv(url, csv_name):
     os.system(f"wget {url} -O {csv_name}")
 
 
-@task
-def clean_taxi(csv_name):
-    df = pd.read_parquet(csv_name)
+@task(name="Schema Typing")
+def pre_taxi(csv_name):
+    if csv_name.split(".")[-1] == "csv":
+        df = pd.read_csv(csv_name)
+    else:
+        df = pd.read_parquet(csv_name)
 
     df = df.rename(
         {
@@ -74,7 +82,19 @@ def clean_taxi(csv_name):
     return df
 
 
-@task(log_prints=True, retries=3)
+@task(name="Transform")
+def transform_data(df: pd.DataFrame):
+    # Do some transforms
+    return df
+
+
+@task(
+    name="Ingest to PostGreSQL",
+    log_prints=True,
+    retries=3,
+    cache_key_fn=task_input_hash,
+    cache_expiration=timedelta(days=1),
+)
 def ingest_data(df, table_name, n_chunks, engine):
     df.head(0).to_sql(name=table_name, con=engine, if_exists="replace")
 
@@ -98,6 +118,28 @@ pd.set_option("display.max_rows", 15)
 pd.set_option("display.max_column", 10)
 
 
+@task(name="Create Minio block")
+def create_minio_block(user="admin", password="password"):
+    MinIOCredentials(minio_root_user=user, minio_root_password=password).save(
+        "minio-default"
+    )
+
+    minio_credentials = MinIOCredentials.load("minio-default")
+    s3_bucket = S3Bucket(bucket_name="data", minio_credentials=minio_credentials)
+    return s3_bucket
+
+
+@flow(name="Ingest to MinIO")
+def ingest_minio(df: pd.DataFrame):
+    minio_block = create_minio_block()
+    path = "transformed.parquet"
+    df.to_parquet(path)
+
+    minio_block.upload_from_path(from_path=path, to_path=Path(f"parquet/{path}"))
+
+    return
+
+
 @flow(name="Ingest Flow")
 def main():
     parser = argparse.ArgumentParser(description="Ingest Parquet to PostGreSQL")
@@ -109,7 +151,7 @@ def main():
     # database name
     # table name
     # url of the csv
-    # number of chunks, default 100000
+    # number of chunks, default 0 which is no chunking
 
     parser.add_argument("--user", help="user for PostGres")
     parser.add_argument("--password", help="password for PostGres")
@@ -117,17 +159,24 @@ def main():
     parser.add_argument("--port", help="port for PostGres")
     parser.add_argument("--db", help="database name for PostGres")
     parser.add_argument("--table_name", help="table name for PostGres")
-    parser.add_argument("--url", help="url of the csv for PostGres")
-    parser.add_argument("--chunks", help="url of the csv for PostGres", default=0)
+    parser.add_argument("--url", help="url of the data")
+    parser.add_argument(
+        "--chunks",
+        help="size of chunks to break the data into, default is whole",
+        default=0,
+    )
 
     args = parser.parse_args()
-
-    csv_name = "output.csv"
-
     table_name, url, n_chunks, engine = connection(args)
+
+    csv_name = f'output.{url.split(".")[-1]}'
+
     download_csv(url, csv_name)
-    df = clean_taxi(csv_name)
-    ingest_data(df, table_name, n_chunks, engine)
+    df = transform_data(pre_taxi(csv_name))
+
+    # path = Path(f'data/{csv_name}')
+    # ingest_data(df, table_name, n_chunks, engine)
+    ingest_minio(df)
 
 
 if __name__ == "__main__":
